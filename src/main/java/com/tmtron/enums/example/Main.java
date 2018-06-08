5058091bc1e3f76d87230257322966a24a5c1ad4
TMTron <martin.trummer@tmtron.com>
2018-06-08 12:04:04 +0200

    added a main-method

--- src/main/java/com/tmtron/enums/example/Main.java
+++ src/main/java/com/tmtron/enums/example/Main.java
@@ -1,8 +1,19 @@
 package com.tmtron.enums.example;
 
+import com.tmtron.enums.EnumMapperFull;
 import com.tmtron.enums.EnumMappers;
 
 // this project only includes tests
 @EnumMappers({ColorEnum.class, BoolEnum.class})
 public class Main {
+
+    public static void main(String[] args) {
+        EnumMapperFull<ColorEnum, String> fullColorMapper =
+                ColorEnum_MapperFull
+                        .setRED("red")
+                        .setBLUE("blue")
+                        .setGREEN("green");
+
+        System.out.println("Green value = " + fullColorMapper.getValue(ColorEnum.GREEN));
+    }
 }
